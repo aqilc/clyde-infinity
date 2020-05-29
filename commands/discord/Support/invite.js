ef7a062be165956857f97e505fb38bce7514c771
Aqil Contractor <aqilcm@gmail.com>
2020-05-29 14:42:04 -0400

    invite command :D

---
+++ commands/discord/Support/invite.js
@@ -0,0 +1,16 @@
+module.exports = {
+  
+    f: (m, { embed }) => m.channel.send(embed.a("Invite me!", m.author.avatarURL(), "https://discord.com/oauth2/authorize?client_id=606986648772935691&scope=bot").f("(click on the \"Invite me!\" thing")),
+    
+    // Description
+    d: "Lets you invite Clyde to your server!",
+    
+    // How to utilize
+    u: " [command_name]",
+    
+    // Example
+    e: "invite",
+    
+    // Channels it works in(a for all, d for dms, and t for guild channels) 
+    c: "a",
+  }
