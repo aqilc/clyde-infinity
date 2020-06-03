7a65320db07008aa3aa0354ca5335841ce5ccaf3
Aqil Contractor <aqilcm@gmail.com>
2020-06-03 18:37:35 -0400

    i tried ;-;

--- commands/discord/Bot Management/recmd.js
+++ commands/discord/Bot Management/recmd.js
@@ -7,24 +7,20 @@ export default {
 	async f(m, { content, embed }) {
 
 		// Finds command based on content
-		let command = this.commands.findIndex(c => c.name === content),
-				cmd = new Object(this.commands[command]);
+		let command = this.commands.find(c => c.name === content);
 
 		// If it can't find the command, tell the user it can't
-		if(command < 0)
+		if(!command)
 			return (await m.channel.send("Command not found.")).delete({ timeout: 5000 });
-
-		// Deletes the cache in require so we can do a full refresh later
-		delete require.cache[require.resolve(cmd.path)];
 
 		// For catching errors
 		try {
 
 			// Gets and makes a new command :D
-			this.commands[command] = await (new Command(cmd.name, cmd.category)).load();
+			this.commands[command] = await command.load();
 
 			// Logs that it was successful
-			console.log(`Refreshed command: ${cmd.name}`);
+			console.log(`Refreshed command: ${command.name}`);
 
 			// Sends a message confirming that the command was done.
 			return (await m.channel.send(embed.a("Command refresh successful!", m.author.avatarURL()))).delete({ timeout: 5000 });
