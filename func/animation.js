c86729d5798cf2f97149b76658ce198e52263681
Aqil Contractor <aqilcm@gmail.com>
2020-06-09 17:00:51 -0400

    Added a way to end the animation

--- func/animation.js
+++ func/animation.js
@@ -2,8 +2,8 @@ import Events from "./events.js";
 
 /**
  * Starts an animation that is run completely by events :D
- * @extends Events
  * @class
+ * @extends Events
  * @param {number} $0.framerate - Framerate of the animation
  * @param {number} $0.delay - How long to delay the start of the animation
  * @fires Animation#start
@@ -16,6 +16,9 @@ export class Animation extends Events {
     // Holds interval later on and tells if the animation is paused or not
     #interval;
 
+    // Tells if the animation is done or not
+    #ended = false;
+
     // Constructor
     constructor ({ framerate = 1500, delay } = {}) {
 
@@ -27,13 +30,12 @@ export class Animation extends Events {
          * @type {this}
          */
         // Delays start or executes immediately based on the 'delay' value. Also holds onto the functions in case you want to cancel 
-        if(!delay) this.delay = setImmediate(() => { this.emit("start", this); this.started = true; this.start(); delete this.delay; })
+        if(!delay) this.delay = setImmediate(() => { this.start(); delete this.delay; })
         else this.delay = setTimeout(() => { this.start(); delete this.delay; }, delay)
     }
 
     /**
      * Starts the 'animation' based on framerate set at start
-     * @method
      * @returns {Animation}
      */
     start() {
@@ -41,9 +43,9 @@ export class Animation extends Events {
         // Sets an interval per set framerate if the animation isn't paused
         if (!this.#interval) {
             if (!this.started)
-                this.emit("start", this), this.started = true;
-            else this.emit("unpaused")
-            this.#interval = setInterval(() => this.emit("draw", this), this.framerate);
+                this.emit("start"), this.started = true;
+            else this.emit("unpaused");
+            this.#interval = setInterval(() => this.emit("draw"), this.framerate);
         }
 
         // Return the object so you can do more with it
@@ -58,9 +60,35 @@ export class Animation extends Events {
 
         // Stops the animation if the animation is running
         if(this.#interval)
-            this.emit("paused", this), clearInterval(this.#interval), this.#interval = false;
+            this.emit("paused"), clearInterval(this.#interval), this.#interval = false;
 
         // Returns instance so you can chain more crap
+        return this;
+    }
+
+    /**
+     * Ends the Animation
+     * @returns {Animation}
+     */
+    end() {
+
+        // Deletes 'start' and 'stop' so you can't start it again
+        delete this.start; delete this.stop;
+
+        // Clears the interval if it still exists
+        if(this.#interval)
+            clearInterval(this.#interval), this.#interval = false;
+
+        // Emits the last event, "end"
+        this.emit("end");
+
+        // Sets 'ended' to true
+        this.#ended = true;
+
+        // Deletes 'end' so you can't break everything
+        delete this.end;
+
+        // Returns instance
         return this;
     }
 
@@ -68,16 +96,11 @@ export class Animation extends Events {
      * Pauses or resumes the animation
      * @public
      * @param {Boolean} bool - Determines state of animation
-     * @returns {Animation}
      */
     set paused(bool) {
 
-        // If 'bool' is actually a boolean, stop and start the animation accordingly
-        if (bool instanceof Boolean)
-            bool && this.stop() || this.start();
-        
-        // Return this object so you can do more stuff
-        return this;
+        // Stop or start based on what 'bool' is
+        !!bool && this.stop() || this.start();
     }
 
     /**
@@ -85,4 +108,10 @@ export class Animation extends Events {
      * @returns {Boolean}
      */
     get paused() { return !!this.#interval }
+
+    /**
+     * Returns if the animation ended or not
+     * @returns {Boolean}
+     */
+    get ended() { return this.#ended }
 }
