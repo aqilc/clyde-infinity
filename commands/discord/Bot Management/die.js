4dc45da801978f97a8494661e7a7822003f9a6b4
Aqil Contractor <aqilcm@gmail.com>
2020-06-10 19:25:12 -0400

    die command initial commit

---
+++ commands/discord/Bot Management/die.js
@@ -0,0 +1,21 @@
+import colors from "colors";
+
+export default {
+
+	f(m) {
+
+		// Kewl message X3
+		const by = m.author.username + "#" + m.author.discriminator,
+					surround =             ("!!!!!!!!!!!!!!!!!!!!!!!!!!!" + "!".repeat(by.length)).bgRed.bold;
+		console.log(surround + "\n" + ` RIP, I'm being killed by ${by} `.bgRed.bold + "\n" + surround);
+
+		// Exits current process
+		process.exit()
+	},
+    
+    // Only bot admins can do this crap
+	p: "BOT_ADMIN",
+
+	// Only developers and mods should have access to this so we hide the command and delete the message invoking it
+	h: true,	del: true,
+}
