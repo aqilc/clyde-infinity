2f739cb7d4f84b6bb4fc229ab27d6be8a0929014
Aqil Contractor <aqilcm@gmail.com>
2020-06-19 22:07:59 -0400

    gets all commands :D

--- func/commands.js
+++ func/commands.js
@@ -1,12 +1,50 @@
 // Need this to read directories
-import { readdir } from "./f";
+import { readdir } from "./f.js";
 
-export const dirs = import.meta.url;
+// Need this for the places to read directories from
+import { project } from "../config.js";
 
-export function get() {
-    
+// Constructs commands
+import Command from "./command.js";
+
+// Gets all commands
+export async function get(type = "discord", commands) {
+  
+  // You need to specify type and all of your commands for it to work xP
+  if(!commands)
+    throw new Error("You need to specify the actual commands you want to get!");
+
+  // Changes a string into an array
+  if(typeof commands === "string")
+    commands = [commands];
+
+  // Checks if the commands are iterable
+  if(!Array.isArray(commands))
+    throw new Error("Commands need to be of correct type! (string or Array)");
+
+  // Holds the actual commands
+  let cmds = [];
+
+  // Loops through the inputted commands and imports them
+  for(let i of commands)
+    if(i.includes(":"))
+      try { cmds.push(await (new Command(i.slice(i.indexOf(":") + 1), i.slice(0, i.indexOf(":")), type)).load());
+      } catch (err) { console.error(err); continue; }
+
+    else for (let j of category(type, i))
+      try { cmds.push(await (new Command(j, i, type)).load());
+      } catch (err) { console.error(err); continue; }
+
+  // Returns the constructed commands object
+  return cmds;
 }
 
-export function categories() {
-    
+export function category(type = "discord", category) {
+
+  // You need to specify category xP
+  if(typeof category !== "string")
+    throw new Error("Please specify the category you want to get!");
+
+  // Returns the read category
+  return readdir(project.commands.append("/" + type + "/" + category).path).files.map(f => f.name);
 }
