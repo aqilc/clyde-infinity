df5566ed8eee58c1efd04b7d9d9b852cbb329a50
Aqil Contractor <aqilcm@gmail.com>
2020-07-01 23:08:17 -0400

    upgraded redis script :D

--- redis.sh
+++ redis.sh
@@ -1,7 +1,17 @@
 #! /bin/sh
 
-sysctl vm.overcommit_memory=1
-sudo echo never > /sys/kernel/mm/transparent_hugepage/enabled
-sudo echo 511 > /proc/sys/net/core/somaxconn
-cd ../../../../../../usr/src/redis-stable
-redis-server
+# Gets the result of the command 'redis-cli ping'
+res=$(redis-cli ping)
+
+# Checks if the command returned PONG(which means it's running)
+if [ "$res" == "PONG" ] ; then
+  echo "Redis is already running :D"
+
+# If it didn't, start redis
+else
+  sysctl vm.overcommit_memory=1
+  sudo echo never > /sys/kernel/mm/transparent_hugepage/enabled
+  sudo echo 511 > /proc/sys/net/core/somaxconn
+  cd /usr/src/redis-stable
+  redis-server
+fi
