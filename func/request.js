1cf0ef8b0734f38e1d4009ce928c218a7d06bf24
Aqil Contractor <aqilcm@gmail.com>
2020-07-17 18:29:01 -0400

    idek

--- func/request.js
+++ func/request.js
@@ -1,15 +1,16 @@
 
 // HTTPS API
 import https from "https";
-      
-// Exports the new function which returns a Promise
+
+/**
+ * Executes an HTTP request
+ * @param {string | URL} url The url
+ * @returns {Promise<any>} The response
+ */
 export default (url, { method, headers, data } = {}) => new Promise((res, rej) => {
 
   // Starts a new HTTPS request
   let req = https.request(url, { method, headers, data }, r => {
-    
-    // The format of received content determining whether to json parse or not
-    const type = r.headers["content-type"];
 
     // Stores data
     let data = "";
@@ -21,9 +22,9 @@ export default (url, { method, headers, data } = {}) => new Promise((res, rej) =
     r.on("end", () => {
 
       // If the format was JSON, parse(with test) and return
-      if (/^application\/json/.test(type))
+      if (/^application\/json/.test(r.headers["content-type"]))
         try { return res(JSON.parse(data)); }
-        catch(err) { console.log(`JSON Parse on content of type ${type} failed.\nError: ${err}\nData: ${data}`); }
+        catch(err) { console.log(`JSON Parse on content of type ${r.headers["content-type"]} failed.\nError: ${err}\nData: ${data}`); }
 
       // Sends raw data as response
       res(data);
