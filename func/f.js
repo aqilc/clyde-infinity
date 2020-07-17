4e74e412fe616c7b64e4037b6f071f129eace3af
Aqil Contractor <aqilcm@gmail.com>
2020-07-17 18:23:56 -0400

    idek

--- func/f.js
+++ func/f.js
@@ -4,11 +4,12 @@ import crypto from "crypto";
 // Imports filesystem modules
 import { readdirSync } from "fs";
 
-/** Finds all mentions in a string
- * @param {string} str
- * @returns {string}
+/**
+ * Finds all mentions in a string
+ * @param {string} str - The string to get mentions from
+ * @returns {string} All the mentions :D
 */
-export const mentions = (str) => str.replace(/\D/g, "").replace(/\s/g, " ").split(" ").filter(v => v.length === 18);
+export const mentions = str => str.replace(/\D/g, "").replace(/\s/g, " ").split(" ").filter(v => v.length === 18);
 
 /**
  * Finds words(with optional prefixes and suffixes) in a string
@@ -23,13 +24,16 @@ export const word = (str/*string*/, w/*word*/, p/*prefix*/, s/*suffix*/) => type
 // Creates a hash
 export const hash = str => crypto.createHash("sha256").update(str).digest();
 
+// Trims a string start and end based on inputted character to trim(basically the thing used in String.prototype.trim but able to support different chars xd)
+export const trim = (str, char = "\\s\\uFEFF\\xA0", { front = true, back = true} = {}) => str.replace(new RegExp(front ? `^[${char}]+` + back ? `|[${char}]+$` : "" : back && `[${char}]+$`, "g"), "")
+
 // Returns a sentence/phrase in title case
 export const titlecase = str => str.split(" ").map(s => s[0].toUpperCase() + s.slice(1)).join(" ");
 
 /**
  * Converts an object into an array
- * @param {Object} obj 
- * @returns {Array<string, any>}
+ * @param {Object} obj - Object to take apart
+ * @returns {Array<string, any>} An array of key-value pairs
  */
 export const objtoarr = obj => obj.length ? obj : Object.keys(obj).map(k => [k, obj[k]]);
 
@@ -126,25 +130,33 @@ export function codify(str) {
 export const fetch = {
   
   // Fetches a bunch of messages, almost to Infinity, until it encounters some undeletable ones(Input: MessageManager ie. m.channel.messages, Amount)
-  async messages(manager, amount) {
+  async messages(manager, amount, skip) {
     
     if(typeof amount !== "number")
       throw new TypeError("\"amount\" not of correct type")
     
     // Stores messages
-    let m = manager.cache.array();
-    
+    let m = manager.cache.array().filter(msg => !msg.deleted);
+
+    // Skips messages
+    if(skip)
+      skip -= m.length - (m = m.slice(-skip)).length;
+
+    // Return messagecount if it's already cached
+    if(m.length >= amount)
+      return m.slice(-amount);
+
     // Slices off the size we already have cached.
     amount -= m.length;
     
     // Fetches all messages and caches them :D
     while (m.length < amount) {
-      
+
       // Sets up the options for getting messages
       let opts = {
         
         // How many messages should be gotten
-        limit: Math.min(amount - m.length, 100)
+        limit: Math.min(amount + skip, 100)
       };
       
       // If *any* messages are cached, find the oldest one and add it into the options
@@ -152,14 +164,18 @@ export const fetch = {
         opts.before = m.reduce((a, b) => Math.min(a.createdTimestamp, b.createdTimestamp)).id;
       
       // Fetches as many messages as needed before the last message we currently have :D
-      (await manager.fetch(opts)).each(v => m.push(v));
+      m = m.concat((await manager.fetch(opts)));
       
       // If undeletable messages are encountered, filter them out of the cache and exit the loop
       if(m.find(mes => !mes.deletable))
-        { let mess; console.log("filtered", mess = (m = m.filter(mes => mes.deletable)).map(v => v.content), mess.length); break; }
+        { console.log("filtered", m.filter(mes => !mes.deletable).map(m => m.content), m.length); m = m.filter(mes => mes.deletable); break; }
     }
     
-    // Returns the cache
+    // Skips some leftover messages if skip was specified
+    if(skip)
+      return m.slice(-skip);
+
+    // else just returns the leftover cache
     return m;
   }
 };
@@ -206,8 +222,11 @@ export const arr = {
 // Creates a random string of letters
 export const rand = {
 
+  // Creates a random number between 'low' and 'high'
+  num: (low = 0, high = 1, round = false) => round ? Math.round(Math.random() * (high - low) + low) : Math.random() * (high - low) + low,
+
   // Generates a random string
-  str(len, { special, nums }) {
+  str(len = 10, { special = false, nums = false }) {
 
     // The returned string
     let str = "",
@@ -243,9 +262,8 @@ export const similarity = {
     
     // Returns custom levenshtein algorithm function
     return function(a, b) {
-      if (a === b) {
+      if (a === b)
         return 0;
-      }
 
       if (a.length > b.length) {
         let tmp = a;
@@ -253,26 +271,22 @@ export const similarity = {
         b = tmp;
       }
 
-      let la = a.length;
-      let lb = b.length;
+      let la = a.length,
+          lb = b.length;
 
-      while (la > 0 && (a.charCodeAt(la - 1) === b.charCodeAt(lb - 1))) {
-        la--;
-        lb--;
-      }
+      while (la > 0 && (a.charCodeAt(la - 1) === b.charCodeAt(lb - 1)))
+        la--, lb--;
 
       let offset = 0;
 
-      while (offset < la && (a.charCodeAt(offset) === b.charCodeAt(offset))) {
-        offset++;
-      }
+      while (offset < la && (a.charCodeAt(offset) === b.charCodeAt(offset)))
+        offset ++;
 
       la -= offset;
       lb -= offset;
 
-      if (la === 0 || lb < 3) {
+      if (la === 0 || lb < 3)
         return lb;
-      }
 
       let x = 0;
       let y;
@@ -318,7 +332,7 @@ export const similarity = {
         }
       }
 
-      for (; x < lb;) {
+      while (x < lb) {
         bx0 = b.charCodeAt(offset + (d0 = x));
         dd = ++x;
         for (y = 0; y < len; y += 2) {
