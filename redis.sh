5b29b8ea0deb45a223d45c6430222171c9e11b16
Aqil Contractor <aqilcm@gmail.com>
2020-10-06 21:09:26 -0400

    small updates for new comp

--- redis.sh
+++ redis.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
 # Gets the result of the command 'redis-cli ping'
-res=$(redis-cli ping)
+#res=$(redis-cli ping)
 
-# Checks if the command returned PONG(which means it's running)
-if [ "$res" == "PONG" ] ; then
+# Checks if the command "redis-cli ping" returned PONG(which means it's running)
+if [ "$(redis-cli ping)" == "PONG" ] ; then
   echo "Redis is already running :D"
 
 # If it didn't, start redis
@@ -12,6 +12,6 @@ else
   sysctl vm.overcommit_memory=1
   sudo echo never > /sys/kernel/mm/transparent_hugepage/enabled
   sudo echo 511 > /proc/sys/net/core/somaxconn
-  cd /usr/src/redis-stable
+  cd /home/aqilc/redis-stable
   redis-server --daemonize yes
 fi
