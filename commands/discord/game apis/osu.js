354928e40be60955c5c7a88c85e723c4513d07b4
Aqil Contractor <aqilcm@gmail.com>
2020-10-06 22:13:48 -0400

    idk mate, maybe stops breakage

--- commands/discord/game apis/osu.js
+++ commands/discord/game apis/osu.js
@@ -4,10 +4,10 @@ import { mentions } from "../../../func/f.js";
 
 // Contains some handy osu functions
 const osu = {
-  
+
   // Creates an osu embed
   embed: (m, user, embed) => {
-    
+
     // User join date
     const join = new Date(user.join_date),
 
@@ -31,9 +31,9 @@ const osu = {
 
         // The first parts
         (progress >= 1 ? "<:bar1:733890019382657035>" + "<:bar4:733895993631834114>".repeat(progress - 1) : "<:bar2:733893420656885771>") +
-        
+
         (progress >= 1 && max - progress > 1 ? "<:bar3:733893342491836428>" : max - progress <= 1 ? "<:bar4:733895993631834114>" : "<:pog6:714449801269477428>") +
-        
+
         (max - progress > 1 ? "<:bar5:733899071076696074>".repeat(max - progress - 2) + "<:bar6:733898929967726634>" : "<:bar7:733897821224304652>") + ` **Lvl ${Math.floor(user.level)}**`)
 
       // Shows PP
@@ -53,7 +53,7 @@ const osu = {
 
       // Shows how many circles you clicked
       .af(`Clicked on ${circles.toLocaleString()} circles`, `${Number(user.count300).toLocaleString()}<:hit300:714227924890288220>  ${Number(user.count100).toLocaleString()}<:hit100:714227924768784384>  ${Number(user.count50).toLocaleString()}<:hit50:714227924387233924>`)
-      
+
       // Sets footer which shows month you joined osu
       .f(`Joined in ${["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"][join.getMonth()]} ${join.getFullYear()}`));
   }
@@ -64,51 +64,51 @@ export default {
 
   // Command versions(optional)
   versions: {
-    
+
     // Version name: Properties to update
     lightweight: {
-      
+
       // Version function (Required)
       async f(m, { content, embed }) {
-        
+
         // Gets user based on message mentions
         let mention = mentions(content)[0];
         if(mention) content = (await this.apis.redis.get(`discord.users[${mention}]:osu.username`)) || "";
-        
+
         // Returns for now since there is no db currently
         if(!content)
           content = (await this.apis.redis.get(`discord.users[${m.author.id}]:osu.username`)) || "";
-        
+
         // If you don't even have a default username setup, just return a message saying you should
         if(!content)
           return m.channel.send(embed.a("Please set up a default username!", m.author.avatarURL()).d(`You can set one up easily with \`${this.prefix}osu su\`, or get quick stats with \`${this.prefix}osu [player name]\`.`))
-        
+
         // Determines and executes command option things
         let option;
         if([1, 2].includes((option = content.slice(0, content.indexOf(" ") >= 1 ? content.indexOf(" ") : content.length)).length))
-          
+
           // Options for the osu command
           return (new Proxy({
-            
+
             // Set user
-            su: async () => {
-              
+            se: async () => {
+
               // Gets user to set your profile into
               let user = content.slice(2).trim();
-              
+
               // If user is unspecified, tell the user to specify it
               if(!user) {
-                
+
                 // Send a message in the channel to tell the user what to do
                 let start = await m.channel.send(embed.t("Send a message containing your osu username, or 'c' to cancel").f("Specified username should at least be greater than 2 characters"));
-                
+
                 // Start a message collector
                 try { user = (await m.channel.awaitMessages(msg => msg.author.id === m.author.id && (msg.content === "c" || msg.content.length > 2), { max: 1, time: 20000, errors: ["time"] })).first().content; }
-                
+
                 // If the author doesn't send another message, the request times out
                 catch(err) { return await m.channel.send(start.edit(embed.a("Timed out", m.author.avatarURL()))); }
               }
-              
+
               // If the user sent a message stating they wanted to cancel, cancel the query
               if(user === "c")
                 return m.channel.send(embed.a('Query Cancelled', m.author.avatarURL()).f(""))
@@ -119,34 +119,34 @@ export default {
               // If user doesn't exist, return with a message.reply
               if(!data)
                 return m.channel.send(embed.a(`User '${user}' doesn't exist!`, m.author.avatarURL()).f(''));
-              
+
               // Sets default username in redis database
               await this.apis.redis.set(`discord.users[${m.author.id}]:osu.username`, data.username);
-              
+
               // Sends a message confirming save
               return m.channel.send(embed.t(`Default username set to ${data.username}!`).url("https://osu.ppy.sh/u/" + data.user_id).f("Now you only have to do '" + this.prefix + "osu' to see your osu! profile :D"))
             }
           }, {
             get(t, p) {
-              
+
               // If the author is able to type, do the correct thing
               if(t[p]) return t[p];
-              
+
               // else tell them that they were bad bois
-              return () => m.channel.send(embed.a("That osu command option doesn't exist!", m.author.avatarURL()))
+              return () => m.channel.send(embed.t("That osu command option doesn't exist!"));
             }
           }))[option]();
-        
+
         // Gets the user stats
         const user = (await this.apis.osu.user(content))[0];
-        
+
         // If user doesn't exist, return with a message.reply
         if(!user)
           return m.channel.send(embed.t("User doesn't exist!"));
-        
+
         // Creates an osu user embed
         return osu.embed(m, user, embed);
-        
+
       }
     }
   },
